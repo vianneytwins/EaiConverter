64e5e9e0bb66fc7e20a0d4457b3145c96106e3bd
vianneytwins <vianneytwins@yahoo.fr>
2015-03-18 23:12:23 +0100

    solution and project files
    solution and project files were missing. I’ve added them

---
+++ EaiConverter/Mapper/JdbcQueryActivityMapper.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using TibcoBWConverter.Model;
+using TibcoBWConverter.CodeGenerator;
+using System.Text;
+using TibcoBWConverter.CodeGenerator.utils;
+
+namespace TibcoBWConverter.Mapper
+{
+	public class JdbcQueryActivityMapper
+	{
+		public JdbcQueryActivityMapper ()
+		{
+		}
+
+		private const string bodyMethodStart = "#using (var db = this.dataAccessFactory.CreateAccess())\n#{\n";
+		private const string dbQuery = "db.Query";
+
+		const string voidString = "void";
+
+		private Dictionary<string,string> sqlMapping = new Dictionary <string, string> {
+			{"VARCHAR","string"},
+			{"INT","int"},
+			{"12","string"},
+			{"4", "int"}
+		};
+		private Dictionary<string,string> jdbcSharedConfigMapping = new Dictionary <string, string> {
+			{"/Configuration/DAI/PNO/JDBC/JDBCIntegration.sharedjdbc","[IntegrationDatabase]"},
+			{"/Configuration/DAI/PNO/JDBC/JDBCPanorama.sharedjdbc","[PanoramaDatabase]"},
+			{"/Configuration/DAI/PNO/JDBC/JDBCPanoramaMasterFiles.sharedjdbc","[MasterFilesDatabase]"}
+		};
+
+		public List<ClassModel> Build (JdbcQueryActivity jdbcQueryActivity)
+		{
+			var classesToGenerate = new List<ClassModel> ();
+			var dataAccessClass = this.BuildDataAccess (jdbcQueryActivity);
+			var dataAccessInterface = InterfaceExtractorFromClass.Extract (dataAccessClass);
+			dataAccessClass.InterfacesToImplement = new List<string> { dataAccessInterface.Name };
+			classesToGenerate.Add(dataAccessClass);
+			classesToGenerate.Add(dataAccessInterface);
+
+			return classesToGenerate;
+		}
+
+		public ClassModel BuildDataAccess (JdbcQueryActivity jdbcQueryActivity)
+		{
+			var dataAccessToGenerate = new ClassModel ();
+			dataAccessToGenerate.Name = VariableHelper.ToClassName (jdbcQueryActivity.Name) + "DataAccess";
+			dataAccessToGenerate.Namespace = "DataAccess";
+
+			dataAccessToGenerate.Imports = this.GenerateImport (jdbcQueryActivity);
+			dataAccessToGenerate.Fields = this.GenererateFields (jdbcQueryActivity);
+			dataAccessToGenerate.Constructors = this.GenererateContructors (jdbcQueryActivity, dataAccessToGenerate);
+			dataAccessToGenerate.Methods = this.GenererateMethods(jdbcQueryActivity);
+
+			return dataAccessToGenerate;
+		}
+
+
+		List<ClassParameter> GenererateFields (JdbcQueryActivity jdbcQueryActivity)
+		{
+			var fields = new List<ClassParameter> {
+				new ClassParameter {
+					Name = "sqlQueryStatement",
+					Type = "string",
+					IsAConstant = true,
+					DefaultValue = "\""+ jdbcQueryActivity.QueryStatement +"\""
+				},
+				new ClassParameter {
+					Name = "dataAccessFactory",
+					Type = "IDataAccessFactory",
+					IsReadOnly = true
+				},
+			};
+			return fields;
+		}
+
+		List<Method> GenererateMethods (JdbcQueryActivity jdbcQueryActivity)
+		{
+			return new List<Method>{ this.GenerateExecuteQueryMethod(jdbcQueryActivity)};
+		}
+
+		List<Method> GenererateContructors (JdbcQueryActivity jdbcQueryActivity, ClassModel classModel)
+		{
+			var constructor = new Method ();
+			constructor.IsPublic = true;
+			constructor.ReturnParameter = new ClassParameter {
+				Name = string.Empty,
+				Type = classModel.Name
+			};
+
+			var inputParameters = new List<ClassParameter> ();
+			var sb = new StringBuilder ();
+
+			sb.AppendLine (string.Format ("#this.{0} = {0};", classModel.Fields [1].Name));
+				inputParameters.Add (new ClassParameter {
+				Name = classModel.Fields[1].Name,
+				Type = classModel.Fields[1].Type,
+				SpecialOption = this.ConvertJDBCConnectionName (jdbcQueryActivity.JdbcSharedConfig)
+				});
+
+			constructor.InputParameters = inputParameters;
+			constructor.MethodBody = sb.ToString ();
+
+			return new List<Method> { constructor };
+		}
+
+		Method GenerateExecuteQueryMethod (JdbcQueryActivity jdbcQueryActivity)
+		{
+			var method = new Method ();
+			method.Name = "ExecuteQuery";
+			//TODO 
+			method.ReturnParameter = new ClassParameter{
+				Type = ConvertSQLTypeToObjectType(jdbcQueryActivity.QueryOutputCachedSchemaDataTypes.ToString()),
+				Name = string.Empty
+			};
+			method.IsPublic = true;
+			method.InputParameters = this.ConvertQueryStatementParameter(jdbcQueryActivity.QueryStatementParameters);
+
+			this.GenerateExecuteQueryBody (jdbcQueryActivity, method);
+
+
+			return method;
+		}
+
+		public void GenerateExecuteQueryBody (JdbcQueryActivity jdbcQueryActivity, Method method)
+		{
+			var sb = new StringBuilder ();
+			sb.Append (bodyMethodStart);
+			var tabulation = new Tab ();
+			if (method.ReturnParameter.Type == voidString) {
+				sb.AppendLine (string.Format ("#{0}(", dbQuery));
+			}
+			else {
+				sb.AppendLine (string.Format ("#return {0}<{1}> (", dbQuery, method.ReturnParameter.Type));
+			}
+			tabulation.Increment ();
+			sb.AppendLine (string.Format ("#{0}\"{1}\",", tabulation, jdbcQueryActivity.QueryStatement));
+			if (method.InputParameters != null && method.InputParameters.Count >= 1) {
+				sb.AppendLine (string.Format ("#{0}new", tabulation));
+				sb.AppendLine (string.Format ("#{0}{{", tabulation.Increment ()));
+				tabulation.Increment ();
+				foreach (var inputParameter in method.InputParameters) {
+					sb.AppendLine (string.Format ("#{0}{1} = {1},", tabulation, inputParameter.Name));
+				}
+				// remoev last comma
+				sb.Remove (sb.Length - 2, 1);
+				tabulation.Decrement ();
+				sb.AppendLine (string.Format ("#{0}}}", tabulation));
+			}
+			//ferme le dbQuery
+			tabulation.Decrement ();
+			sb.AppendLine (string.Format ("#{0}).FirstOrDefault();", tabulation));
+			//ferme le using
+			sb.AppendLine ("#}");
+			method.MethodBody = sb.ToString ();
+		}
+
+		List<ClassParameter> ConvertQueryStatementParameter (Dictionary<string,string> queryStatementParameters)
+		{
+			var methodInputParameters = new List<ClassParameter> ();
+			if (queryStatementParameters == null) {
+				return methodInputParameters;
+			}
+			foreach (var queryParam in queryStatementParameters) {
+				methodInputParameters.Add(new ClassParameter {
+					Name = VariableHelper.ToVariableName(queryParam.Key),
+					Type = this.ConvertSQLTypeToObjectType (queryParam.Value)
+				});
+			}
+			return methodInputParameters;
+		}
+				
+		string ConvertSQLTypeToObjectType (string type)
+		{
+			// TODO pour le moment on laisse comme ca car on veut lister tous les types et a mettre dans le dico et donc que cela plente
+			string resultType;
+			if (sqlMapping.TryGetValue (type, out resultType)) {
+				return resultType;
+			}
+			else
+			{
+				return voidString;
+
+			}
+			
+		}
+
+		string ConvertJDBCConnectionName (string type)
+		{
+			string resultType;
+			if (jdbcSharedConfigMapping.TryGetValue (type, out resultType)) {
+				return resultType;
+			}
+			else
+			{
+				return "[PanoramaDatabase]";
+
+			}
+		}
+
+		public List<string> GenerateImport (JdbcQueryActivity jdbcQueryActivity)
+		{
+			return new List<string> {
+				"System",
+				"System.Linq",
+				"Lyxor.Panorama.DataAccess.Common"
+			};
+		}
+
+		void BuildDataAccessInterface (ClassModel dataAccessClass)
+		{
+			//throw new NotImplementedException ();
+		}
+	}
+}
+
---
+++ EaiConverter/Mapper/TibcoBWProcessMapper.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using TibcoBWConverter.CodeGenerator;
+using System.Text;
+using TibcoBWConverter.Model;
+
+namespace TibcoBWConverter.Mapper
+{
+	public class TibcoBWProcessMapper
+	{
+
+		public List<ClassModel> Build (TibcoBWProcess tibcoBwProcessToGenerate){
+
+			var tibcoBwProcessClassModel = new ClassModel ();
+			tibcoBwProcessClassModel.Name = tibcoBwProcessToGenerate.ProcessName;
+
+			tibcoBwProcessClassModel.Namespace = tibcoBwProcessToGenerate.NameSpace;
+
+			// 2 les using : de chaque namespace d'activite -> toujours les meme car peut pas savoir les domaines avant sauf si veut les classer par Type d'activiter
+			tibcoBwProcessClassModel.Imports = this.GenerateImport (tibcoBwProcessToGenerate);
+
+			// 3 les membres privee : les activité injecte
+			tibcoBwProcessClassModel.Fields = this.GeneratePrivateFields (tibcoBwProcessToGenerate);
+
+			// 4 le ctor avec injection des activités + logger
+			tibcoBwProcessClassModel.Constructors = this.GenerateConstructors (tibcoBwProcessToGenerate, tibcoBwProcessClassModel);
+
+			// 5 les properties : ici y en a pas
+			//this.GenerateProperties (tibcoBwProcessToGenerate);
+
+			// 6 la methode start avec input starttype et return du endtype
+			tibcoBwProcessClassModel.Methods = this.GenerateMethod (tibcoBwProcessToGenerate);
+
+			var classModels = new List<ClassModel> { tibcoBwProcessClassModel };
+
+			//7 Mappe les classes des activity
+//			classModels.AddRange (this.GenerateActivityClasses (tibcoBwProcessToGenerate));
+//			if (tibcoBwProcessToGenerate.EndActivity!= null && tibcoBwProcessToGenerate.EndActivity.AdditionnalClassesToGenerate != null) {
+//				classModels.AddRange (tibcoBwProcessToGenerate.EndActivity.AdditionnalClassesToGenerate);
+//			}
+//			if (tibcoBwProcessToGenerate.StartActivity!= null && tibcoBwProcessToGenerate.StartActivity.AdditionnalClassesToGenerate != null) {
+//				classModels.AddRange (tibcoBwProcessToGenerate.StartActivity.AdditionnalClassesToGenerate);
+//			}
+			return classModels;
+		}
+
+		public List<string> GenerateImport (TibcoBWProcess tibcoBwProcessToGenerate)
+		{
+			return new List<string> {
+				"System",
+				TibcoBWConverterConstant.ActivityNameSpace,
+				tibcoBwProcessToGenerate.InputAndOutputNameSpace
+			};
+		}
+
+		public List<ClassParameter> GeneratePrivateFields (TibcoBWProcess tibcoBwProcessToGenerate)
+		{
+			var fields = new List<ClassParameter> ();
+			fields.Add (new ClassParameter {
+				Type = "ILogger",
+				Name= "logger",
+				IsReadOnly = true
+			});
+			foreach (Activity activity in tibcoBwProcessToGenerate.Activities) {
+				fields.Add (new ClassParameter {
+					Type = "I" + VariableHelper.ToClassName (activity.Name + "Service"),
+					Name = VariableHelper.ToVariableName (VariableHelper.ToClassName (activity.Name + "Service")),
+					IsReadOnly = true
+				});
+			}
+
+			return fields;
+		}
+
+		public List<Method> GenerateConstructors (TibcoBWProcess tibcoBwProcessToGenerate, ClassModel classModel)
+		{
+
+			var constructor = new Method ();
+			constructor.IsPublic = true;
+			constructor.ReturnParameter = new ClassParameter {
+				Name = string.Empty,
+				Type = classModel.Name
+			};
+
+			var inputParameters = new List<ClassParameter> ();
+			var sb = new StringBuilder ();
+			foreach (ClassParameter field in classModel.Fields) {
+				sb.AppendLine ("#this." + field.Name + " = " + field.Name + ";");
+				inputParameters.Add (new ClassParameter {
+					Name = field.Name,
+					Type = field.Type
+				});
+			}
+			constructor.InputParameters = inputParameters;
+			constructor.MethodBody = sb.ToString ();
+
+			return new List<Method> { constructor };
+		}
+
+		public void GenerateProperties(TibcoBWProcess tibcoBwProcessToGenerate)
+		{
+		}
+			
+		public List<Method> GenerateMethod (TibcoBWProcess tibcoBwProcessToGenerate)
+		{
+			return new List<Method> {GenerateStartMethod (tibcoBwProcessToGenerate)};
+		}
+
+		// TODO : refactoriser car ca commence a etre tres moche
+		public Method GenerateStartMethod (TibcoBWProcess tibcoBwProcessToGenerate)
+		{
+			var startMethod = new Method ();
+
+			string returnType;
+			string returnName;
+			if (tibcoBwProcessToGenerate.EndActivity ==null || tibcoBwProcessToGenerate.EndActivity.Parameters == null) {
+				returnType = "void";
+				returnName = string.Empty;
+			}
+			else {
+				returnType = tibcoBwProcessToGenerate.EndActivity.Parameters [0].Type;
+				returnName = VariableHelper.ToVariableName (tibcoBwProcessToGenerate.EndActivity.Parameters [0].Name);
+			}
+			startMethod.ReturnParameter = new ClassParameter {
+				Name = returnName,
+				Type = returnType
+			};
+
+			string inputType;
+			string inputName;
+			if (tibcoBwProcessToGenerate.StartActivity != null){
+				startMethod.Name = tibcoBwProcessToGenerate.StartActivity.Name;
+			}
+
+			if (tibcoBwProcessToGenerate.StartActivity == null || tibcoBwProcessToGenerate.StartActivity.Parameters == null) {
+				inputType = string.Empty;
+				inputName = string.Empty;
+			}
+			else {
+
+				inputType = tibcoBwProcessToGenerate.StartActivity.Parameters [0].Type;
+				inputName = tibcoBwProcessToGenerate.StartActivity.Parameters [0].Name;
+				startMethod.InputParameters = new List<ClassParameter> {
+					new ClassParameter {
+						Name = inputName,
+						Type = inputType
+					}
+				};
+			}
+
+			var sb = new StringBuilder ();
+			if (returnType != "void") {
+				sb.AppendLine (@"#var " + startMethod.ReturnParameter.Name + " = new " + returnType + "();");
+				sb.AppendLine ("#");
+				sb.AppendLine ("#return " + startMethod.ReturnParameter.Name + ";");
+			}
+			startMethod.MethodBody = sb.ToString();
+
+			return startMethod;
+
+		}
+
+		public List<ClassModel> GenerateActivityClasses (TibcoBWProcess tibcoBwProcessToGenerate)
+		{
+			//TODO factory
+			var jdbcQueryActivityMapper = new JdbcQueryActivityMapper ();
+			var activityClasses = new List<ClassModel> ();
+			foreach (var activity in tibcoBwProcessToGenerate.Activities) {
+				if (activity.Type == JdbcQueryActivity.jdbcQueryActivityType) {
+					activityClasses.AddRange (jdbcQueryActivityMapper.Build ((JdbcQueryActivity) activity));
+				}
+			}
+			return activityClasses;
+		}
+	}
+}
+
