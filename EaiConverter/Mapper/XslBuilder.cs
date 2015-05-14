5a4e73455579a1aa9e23072bdd51f186119bc6d7
vianneytwins <vianneytwins@yahoo.fr>
2015-05-14 22:18:41 +0200

    Add XslBuilder

---
+++ EaiConverter/Mapper/XslBuilder.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Text;
+using System.CodeDom;
+
+namespace EaiConverter.Mapper
+{
+    public class XslBuilder
+    {
+        XNamespace xslNameSpace = "http://w3.org/1999/XSL/Transform";
+
+        public List<string> Build (IEnumerable<XNode> inputNodes, string parent){
+
+            var codeStatements = new List<string>();
+
+            foreach(var inputNode in inputNodes)
+            {
+                var element = (XElement) inputNode;
+                if (element.Name.NamespaceName != this.xslNameSpace) {
+                    string returnType = this.DefineReturnType(element);
+                    string variableReference = this.DefineVariableReference(element,parent);
+                    if (this.IsBasicReturnType(returnType))
+                    {
+                        codeStatements.Add(variableReference + element.Name + " = ");
+                        codeStatements.AddRange(this.Build(element.Nodes(),parent));
+                    }
+                    else
+                    {
+                        // intialise the variable first
+                        codeStatements.Add(returnType + " " + element.Name + " = new " +returnType + "();\n") ;
+                        codeStatements.AddRange(this.Build(element.Nodes(), element.Name.ToString()));
+                    }
+
+                } else {
+                    codeStatements.Add(@""""+ element.Attribute("select").Value + @"""" + ";\n");
+                }
+
+            }
+            return codeStatements;
+
+        }
+
+        public CodeStatementCollection Build (IEnumerable<XNode> inputNodes){
+            var codeInStringList = this.Build(inputNodes, null);
+            var codeSnippet = new CodeSnippetStatement (GenerateCode(codeInStringList));
+            var codeStatements = new CodeStatementCollection();
+            codeStatements.Add(codeSnippet);
+            return codeStatements;
+        }
+
+        public bool IsBasicReturnType(string returnType)
+        {
+            switch (returnType)
+            {
+                case "string":
+                    return true;
+                case "double":
+                    return true;
+                case "bool":
+                    return true;
+                case "DateTime":
+                    return true;
+                default:
+                    return false;
+            }
+
+        }
+
+        public string DefineReturnType(XElement inputedElement)
+        {
+            var elementTypes = new List<string>();
+            var nodes = new List<XNode>();
+            nodes.Add(inputedElement);
+            this.RetrieveAllTypeInTheElement(nodes, elementTypes);
+            if (IsBasicReturnType(elementTypes[1]))
+            {
+                return elementTypes[1];
+            }
+            return elementTypes[0];
+        }
+
+        string DefineVariableReference(XElement inputedElement, string parent)
+        {
+            var elementTypes = new List<string>();
+            var nodes = new List<XNode>();
+            nodes.Add(inputedElement);
+            this.RetrieveAllTypeInTheElement(nodes, elementTypes);
+            if (parent == null && elementTypes.Count == 2)
+            {
+                return elementTypes[1] + " ";
+            } 
+            return parent+ ".";
+        }
+
+        private void RetrieveAllTypeInTheElement(IEnumerable<XNode> inputedElement, List<string> elementTypes)
+        {
+            foreach (XElement item in inputedElement)
+            {
+                if (item.Name.NamespaceName != this.xslNameSpace)
+                {
+                    elementTypes.Add(item.Name.ToString());
+                }
+                else
+                {
+                    if (item.Attribute("select").Value.Contains("tib:parse-dateTime"))
+                    {
+                        elementTypes.Add("DateTime");
+                    
+                    } else if (item.Attribute("select").Value.StartsWith("number")){
+                        elementTypes.Add("double");
+                    }
+
+                    else{
+                        elementTypes.Add("string");
+                    }
+                }
+                if (item.HasElements)
+                {
+                    this.RetrieveAllTypeInTheElement(item.Nodes(), elementTypes);
+                }
+            }
+        }
+
+        string GenerateCode(List<string> codeStatement)
+        {
+            var generatedCode = new StringBuilder();
+            foreach (var item in codeStatement)
+            {
+                generatedCode.Append(item);
+            }
+            return generatedCode.ToString();
+        }
+
+}
+}
+
--- EaiConverterTest/Test/XslBuilderTest.cs
+++ EaiConverterTest/Test/XslBuilderTest.cs
@@ -2,20 +2,143 @@
 using NUnit.Framework;
 using System.Xml;
 using System.Xml.Xsl;
+using System.Xml.Linq;
+using EaiConverter.Mapper;
+using System.Collections.Generic;
+using System.Text;
+using EaiConverter.Test.Utils;
 
-namespace EaiConverter
+namespace EaiConverterTest
 {
 	[TestFixture]
 	public class XslBuilderTest
 	{
-		[Ignore]
-		[Test]
-		public void Should_Merge_2_Files_in_1_With_XSLT(){
-			XslTransform myXslTransform; 
-			myXslTransform = new XslTransform();
-			myXslTransform.Load("books.xsl"); 
-			myXslTransform.Transform("adminFormat.xml", "NTM.xml"); 
-		}
+        XslBuilder xslBuilder;
+   
+
+        [SetUp]
+        public void SetUp ()
+        {
+            xslBuilder = new XslBuilder ();
+
+        }
+
+        [Test]
+        public void Should_Return_1_Variable_assignement_with_String_Type (){
+            var xml =
+                @"<pd:inputBindings xmlns:pd=""http://xmlns.tibco.com/bw/process/2003"" xmlns:xsl=""http://w3.org/1999/XSL/Transform"">
+        <FundName>
+            <xsl:value-of select=""testvalue""/>
+        </FundName>
+</pd:inputBindings>
+";
+            XElement doc = XElement.Parse(xml);
+
+            var codeStatement = xslBuilder.Build (doc.Nodes());
+
+            string generateCode = TestCodeGeneratorUtils.GenerateCode(codeStatement);
+            Assert.AreEqual (@"string FundName = ""testvalue"";
+
+", generateCode);
+        }
+
+        [Test]
+        [Ignore]
+        public void Should_Return_1_Variable_assignement_with_double_Type (){
+            var xml =
+                @"<pd:inputBindings xmlns:pd=""http://xmlns.tibco.com/bw/process/2003"" xmlns:xsl=""http://w3.org/1999/XSL/Transform"">
+        <FundName>
+            <xsl:value-of select=""number(testvalue)""/>
+        </FundName>
+</pd:inputBindings>
+";
+            XElement doc = XElement.Parse(xml);
+
+            var codeStatement = xslBuilder.Build (doc.Nodes());
+
+            string generateCode = TestCodeGeneratorUtils.GenerateCode(codeStatement);
+            Assert.AreEqual (@"double FundName = double.Parse(""testvalue"");
+
+", generateCode);
+        }
+
+        [Test]
+        [Ignore]
+        public void Should_Return_1_Variable_assignement_with_DateTime_Type (){
+            var xml =
+                @"<pd:inputBindings xmlns:pd=""http://xmlns.tibco.com/bw/process/2003"" xmlns:xsl=""http://w3.org/1999/XSL/Transform"">
+        <FundName>
+            <xsl:value-of select=""tib:parse-dateTime('MMM dd yyyy', $Mystuff)""/>
+        </FundName>
+</pd:inputBindings>
+";
+            XElement doc = XElement.Parse(xml);
+
+            var codeStatement = xslBuilder.Build (doc.Nodes());
+
+            string generateCode = TestCodeGeneratorUtils.GenerateCode(codeStatement);
+            Assert.AreEqual (@"DateTime FundName = DateTime.ParseExact($Mystuff, ""MMM dd yyyy"", null);
+", generateCode);
+
+        }
+
+        [Test]
+        public void Should_Return_2_Variable_assignement_with_string_Type (){
+            var xml =
+                @"<pd:inputBindings xmlns:pd=""http://xmlns.tibco.com/bw/process/2003"" xmlns:xsl=""http://w3.org/1999/XSL/Transform"">
+        <FundName>
+            <xsl:value-of select=""testvalue""/>
+        </FundName>
+        <AdminID>
+            <xsl:value-of select=""EVL""/>
+        </AdminID>
+</pd:inputBindings>
+";
+            XElement doc = XElement.Parse(xml);
+
+            var generateCode = TestCodeGeneratorUtils.GenerateCode(xslBuilder.Build (doc.Nodes()));
+
+            Assert.AreEqual (@"string FundName = ""testvalue"";
+string AdminID = ""EVL"";
+
+", generateCode);
+        }
+
+
+        [Test]
+        public void Should_Return_1_Variable_assignement_with_complex_Type (){
+            var xml =
+                @"<pd:inputBindings xmlns:pd=""http://xmlns.tibco.com/bw/process/2003"" xmlns:xsl=""http://w3.org/1999/XSL/Transform"">
+<sqlParams>        
+<FundName>
+            <xsl:value-of select=""testvalue""/>
+        </FundName>
+        <AdminID>
+            <xsl:value-of select=""EVL""/>
+        </AdminID>
+</sqlParams>  
+</pd:inputBindings>
+";
+            XElement doc = XElement.Parse(xml);
+
+            var generateCode = TestCodeGeneratorUtils.GenerateCode(xslBuilder.Build (doc.Nodes()));
+
+            Assert.AreEqual (@"sqlParams sqlParams = new sqlParams();
+sqlParams.FundName = ""testvalue"";
+sqlParams.AdminID = ""EVL"";
+
+", generateCode.ToString());
+        }
+
+        string GenerateCode(List<string> codeStatement)
+        {
+            var generatedCode = new StringBuilder();
+            foreach (var item in codeStatement)
+            {
+                generatedCode.Append(item);
+            }
+            return generatedCode.ToString();
+        }
 	}
 }
 
