0eeaf29aea33cc8082efd033f4ca495cb2e7b5e3
vianneytwins <vianneytwins@yahoo.fr>
2015-06-02 20:46:26 +0200

    merge change

--- EaiConverter/Parser/JavaActivityParser.cs
+++ EaiConverter/Parser/JavaActivityParser.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Xml.Linq;
 using EaiConverter.Model;
 using EaiConverter.Parser.Utils;
-using EaiConverter.Mapper;
 
 namespace EaiConverter.Parser
 {
@@ -15,18 +14,19 @@ namespace EaiConverter.Parser
             var activity = new JavaActivity ();
 
             activity.Name = inputElement.Attribute ("name").Value;
-            activity.Type = (ActivityType) inputElement.Element (XmlnsConstant.tibcoPrefix + "type").Value;
+            activity.Type = (ActivityType) inputElement.Element (XmlnsConstant.tibcoProcessNameSpace + "type").Value;
+
 			var configElement = inputElement.Element ("config");
 
             activity.FileName = XElementParserUtils.GetStringValue(configElement.Element("fileName"));
             activity.PackageName = XElementParserUtils.GetStringValue(configElement.Element("packageName"));
             activity.FullSource = XElementParserUtils.GetStringValue(configElement.Element("fullsource"));
 
-            activity.InputBindings = inputElement.Element (XmlnsConstant.tibcoPrefix + "inputBindings").Nodes();
+            activity.InputBindings = inputElement.Element (XmlnsConstant.tibcoProcessNameSpace + "inputBindings").Nodes();
 
-            if (inputElement.Element(XmlnsConstant.tibcoPrefix + "inputBindings") != null && inputElement.Element(XmlnsConstant.tibcoPrefix + "inputBindings").Element("javaCodeActivityInput") != null)
+            if (inputElement.Element(XmlnsConstant.tibcoProcessNameSpace + "inputBindings") != null && inputElement.Element(XmlnsConstant.tibcoProcessNameSpace + "inputBindings").Element("javaCodeActivityInput") != null)
             {
-                activity.InputBindings = inputElement.Element(XmlnsConstant.tibcoPrefix + "inputBindings").Element("javaCodeActivityInput").Nodes();
+                activity.InputBindings = inputElement.Element(XmlnsConstant.tibcoProcessNameSpace + "inputBindings").Element("javaCodeActivityInput").Nodes();
                 activity.Parameters = new XslParser().Build(activity.InputBindings);
             }
 
