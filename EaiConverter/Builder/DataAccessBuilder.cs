1af6c8b7e52e2f69c880492d3fd3a663ed8b9368
vianneytwins <vianneytwins@yahoo.fr>
2015-09-05 15:39:03 +0200

    first or single .. what ever

--- EaiConverter/Builder/DataAccessBuilder.cs
+++ EaiConverter/Builder/DataAccessBuilder.cs
@@ -176,6 +176,25 @@
             sb.AppendLine("}");
             return new CodeSnippetStatement(sb.ToString());
         }
+
+        /// <summary>
+        /// Generates the execute query body.
+        /// </summary>
+        /// <returns>The execute query body.</returns>
+        /// <param name="jdbcQueryActivity">Jdbc query activity.</param>
+        /// <param name="method">Method.</param>
+        //public CodeSnippetStatement GenerateExecuteQueryBodyForJdbcCall(JdbcQueryActivity jdbcQueryActivity, CodeMemberMethod method)
+        //{
+        //    var p = new DynamicParameters();
+        //    p.Add("@a", 11);
+        //    p.Add("@b", dbType: DbType.Int32, direction: ParameterDirection.Output);
+        //    p.Add("@c", dbType: DbType.Int32, direction: ParameterDirection.ReturnValue);
+
+        //    cnn.Execute("spMagicProc", p, commandType: CommandType.StoredProcedure); 
+
+        //    int b = p.Get<int>("@b");
+        //    int c = p.Get<int>("@c");
+        //}
     }
 }
 
